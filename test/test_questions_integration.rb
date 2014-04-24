5982aa606466959b7ebb0aea91da8bdabe81bedd
Mitchell Hollander <MitchellSH@live.com>
2014-04-24 16:19:38 -0500

    a test added

--- test/test_questions_integration.rb
+++ test/test_questions_integration.rb
@@ -1,58 +1,21 @@
-calrequire_relative 'helper'
+require_relative 'helper'
 
-class TestCheersIntegration < MiniTest::Unit::TestCase
+class TestQuestionsIntegration < MiniTest::Unit::TestCase
 
-  def test_a_name_with_no_vowels
+  def test_a_name_no_caps
     shell_output = ''
-    IO.popen('ruby cheers.rb', 'r+') do |pipe|
-      pipe.puts 'ZIM'
+    IO.popen('ruby questions.rb', 'r+') do |pipe|
+      pipe.puts 'zim'
       pipe.close_write
       shell_output = pipe.read
     end
     expected_output = <<EOS
-What's your name?
-Give me a... Z
-Give me an.. I
-Give me a... M
+What's your first name?
+Your first name is Zim.
+What's your last name?
 EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_a_name_with_no_caps
-    shell_output = ''
-    IO.popen('ruby cheers.rb', 'r+') do |pipe|
-      pipe.puts 'mitch'
-      pipe.close_write
-      shell_output = pipe.read
-    end
-    expected_output = <<EOS
-What's your name?
-Give me a... M
-Give me an.. I
-Give me a... T
-Give me a... C
-Give me a... H
-EOS
-    assert_equal expected_output, shell_output
-  end
-
-  def test_a_name_with_all_caps
-    shell_output = ''
-    IO.popen('ruby cheers.rb', 'r+') do |pipe|
-      pipe.puts 'MITCH'
-      pipe.close_write
-      shell_output = pipe.read
-    end
-    expected_output = <<EOS
-What's your name?
-Give me a... M
-Give me an.. I
-Give me a... T
-Give me a... C
-Give me a... H
-EOS
-    assert_equal expected_output, shell_output
-  end
-
-
+  
 end
