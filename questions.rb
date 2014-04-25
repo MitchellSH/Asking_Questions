886ba13cc5e3b91e3495cd5fdb903d6bb3f36b24
Mitchell Hollander <MitchellSH@live.com>
2014-04-24 21:57:27 -0500

    added another question

--- questions.rb
+++ questions.rb
@@ -1,4 +1,4 @@
-# Asking all them questions :P 
+# Asking all them questions :P
 puts "What's your first name?"
 first_name = gets.chomp.capitalize
   puts "Your first name is #{first_name}."
@@ -16,6 +16,15 @@ state = gets.chomp.upcase
   puts "You live in the state of #{state}, in the city of #{city}, and your name is #{first_name} #{last_name}."
   puts "This is a simple app!"
 
+puts "Are you an Alien? Enter yes or no."
+input_alien = gets.chomp
+if input_alien == "yes"
+  puts "Sweet! Aliens rock!"
+else
+  puts "Humans are boring but whatever."
+end
+
+
 puts "Want to see how many characters in your first name? Enter yes or no."
 num = first_name.length
 input = gets.chomp
@@ -28,6 +37,5 @@ end
 # The simplest app in the world!
 # Just some extra simple Ruby practice.
 
-
-
-
+# Sometimes to make great apps,
+# you got to first make stupid small ones on the side.
